01f43f5c0ee516b02f660cc6af4fa9b033328850
Aditya Patel <aditya@Adityas-MBP-2.Dlink>
2024-07-28 18:45:06 +0530

    Annotations Added Up Until 2023

--- temp.js
+++ temp.js
@@ -2,11 +2,110 @@ const margin = { top: 70, right: 30, bottom: 40, left: 80 };
 const width = 1450 - margin.left - margin.right;
 const height = 400 - margin.top - margin.bottom;
 
-// Set up the x and y scales
 const x = d3.scaleTime().rangeRound([0, width]);
 const y = d3.scaleLinear().rangeRound([height, 0]);
 
-// Create the SVG element and append it to the chart container
+const annotation_vals = [[
+  {
+    note: {
+      label: "Nvidia's GeForce 10 series, unveiled in 2016, introduced the GTX 1080 and 1070 GPUs with Pascal architecture, outperforming the Titan X and featuring SMP for enhanced multi-monitor and VR rendering.",
+      title: "First GPU Launch",
+      wrap: 200
+    },
+    x: 505,
+    y: 242,
+    dy: -55,
+    dx: -35
+  }
+],[
+  {
+    note: {
+      label: "Intel, dominating the data center market, plans to launch the Xeon Phi processor to boost deep learning by 100 times by 2020.",
+      title: "Nvidia stock affected by Intel",
+      wrap: 200
+    },
+    x: 338,
+    y: 242,
+    dy: -100,
+    dx: -35
+  },
+  {
+    note: {
+      label: "Nvidia officially released the Titan V.",
+      title: "Huge Launch to Pave a Future",
+      wrap: 200
+    },
+    x: 1243,
+    y: 200,
+    dy: -100,
+    dx: -35
+  }
+],[{
+  note: {
+    label: "Nvidia developed an open-source ventilator, announced the Ampere GPU microarchitecture and A100 GPU accelerator.",
+    title: "Developments in GPU Tech",
+    wrap: 200
+  },
+  x: 841,
+  y: 130,
+  dy: -30,
+  dx: -15
+}],[{
+  note: {
+    label: "Nvidia expanded its high-performance computing market with the Mellanox acquisition, launched RTX Studio laptops, and introduced real-time ray tracing in Minecraft.",
+    title: "Laptop Production",
+    wrap: 250
+  },
+  x: 425,
+  y: 200,
+  dy: -100,
+  dx: -35
+}],
+[
+  {
+    note: {
+      label: "Nvidia developed an open-source ventilator, announced the Ampere GPU microarchitecture and A100 GPU accelerator.",
+      title: "Developments in GPU",
+      wrap: 200
+    },
+    x: 196,
+    y: 100,
+    dy: -50,
+    dx: -1
+  }
+],[{
+  note: {
+    label: "Nvidia's top scientists developed an open-source ventilator to address the global coronavirus pandemic shortage. Nvidia also announced their Ampere GPU microarchitecture and the Nvidia A100 GPU accelerator, and entered talks with SoftBank to buy Arm, a UK-based chip designer, for $32 billion.",
+    title: "Covid Causes Problems",
+    wrap: 900
+  },
+  x: 1125,
+  y: 70,
+  dy: -15,
+  dx: -100
+}],[{
+  note: {
+    label: "The proposed takeover of Arm Holdings by Nvidia was stalled due to significant competition concerns raised by the UK’s Competition and Markets Authority. The European Commission also opened a competition investigation, stating that the acquisition could restrict competitors’ access to Arm’s products and give Nvidia too much internal information on its competitors.",
+    title: "Nvidia's Attempted Acquisition of Arm Holdings",
+    wrap: 850
+  },
+  x: 892,
+  y: 70,
+  dy: -15,
+  dx: -15
+}],[{
+  note: {
+    label: "NVIDIA announced six new RTX Ada Lovelace GPUs for laptops and desktops, enhancing AI, design, and metaverse applications.",
+    title: "NVIDIA’s New RTX Ada Lovelace GPUs",
+    wrap: 200
+  },
+  x: 1200,
+  y: 150,
+  dy: -40,
+  dx: -20
+}],[]
+]
+
 const svg = d3.select("#chart-container")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -14,7 +113,6 @@ const svg = d3.select("#chart-container")
   .append("g")
     .attr("transform", `translate(${margin.left},${margin.top})`);
 
-// Create a new SVG element for the second graph and append it to a new container
 const svg2 = d3.select("#chart-container-2")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -22,13 +120,10 @@ const svg2 = d3.select("#chart-container-2")
   .append("g")
     .attr("transform", `translate(${margin.left},${margin.top})`);
 
-// Create an array of years
 const years = ["2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023", "2024"];
 
-// Define a variable to keep track of the current year index
 let currentYearIndex = 0;
 
-// Create a button for each year
 years.forEach(year => {
   d3.select("#buttons")
     .append("button")
@@ -41,57 +136,44 @@ years.forEach(year => {
     });
 });
 
-// Create the "Next" button
 d3.select("#buttons")
   .append("button")
   .attr("class", "year-button")
   .text("Next")
   .on("click", function() {
-    // Increment the current year index
     currentYearIndex++;
 
-    // If the current year index exceeds the length of the years array, reset it to 0
     if (currentYearIndex >= years.length) {
       currentYearIndex = years.length - 1;
     }
 
-    // Get the next year
     const nextYear = years[currentYearIndex];
 
-    // Load data and draw graphs for the next year
     loadDataAndDrawGraph(nextYear);
     loadDataAndDrawGraph2(nextYear);
     loadannotations(nextYear);
   });
 
   function loadDataAndDrawGraph(year) {
-    // Load and Process Data
     d3.csv("nvidia_stock.csv", d3.autoType).then(function(data) {
-      // Filter the data based on the selected year
       const filteredData = data.filter(d => d.date.getFullYear().toString() === year);
   
-      // Clear the existing graph
       svg.selectAll("*").remove();
   
-      // Define the x and y domains
       x.domain(d3.extent(filteredData, d => d.date));
       y.domain([0, d3.max(filteredData, d => d.high)]);
   
-      // Add the x-axis
       svg.append("g")
         .attr("transform", `translate(0,${height})`)
         .call(d3.axisBottom(x).ticks(d3.timeMonth.every(1)).tickFormat(d3.timeFormat('%B %Y')));
   
-      // Add the y-axis
       svg.append("g")
         .call(d3.axisLeft(y));
   
-      // Create the line generator
       const line = d3.line()
         .x(d => x(d.date))
         .y(d => y(d.high));
   
-      // Add the line path to the SVG element
       svg.append("path")
         .datum(filteredData)
         .attr("fill", "none")
@@ -114,7 +196,6 @@ d3.select("#buttons")
         .attr("opacity", .70)
         .style("pointer-events", "none");
   
-      // create a listening rectangle
       const listeningRect = svg.append("rect")
         .attr("width", width)
         .attr("height", height)
@@ -149,7 +230,6 @@ d3.select("#buttons")
           .style("top", `${yPos + 50}px`)
           .html(`<strong>Date:</strong> ${d.date.toLocaleDateString()}<br><strong>High:</strong> ${d.high.toFixed(2)}<br><br><strong>DIGGING DEEPER</strong><br><br><strong>Low:</strong> ${d.low.toFixed(2)}<br><strong>Open:</strong> ${d.open.toFixed(2)}<br><strong>Close:</strong> ${d.close.toFixed(2)}`)
   
-        console.log(xPos)
       });
 
       listeningRect.on("mouseout", function() {
@@ -160,8 +240,6 @@ d3.select("#buttons")
       });      
     });
   }
-  
-const annotationDate = new Date("2019-11-02");
 
 function loadDataAndDrawGraph2(year) {
   d3.csv("nvidia_stock.csv", d3.autoType).then(function(data) {
@@ -172,21 +250,17 @@ function loadDataAndDrawGraph2(year) {
     x.domain(d3.extent(filteredData, d => d.date));
     y.domain([0, d3.max(filteredData, d => d.volume)]);
 
-    // Add the x-axis
     svg2.append("g")
       .attr("transform", `translate(0,${height})`)
       .call(d3.axisBottom(x).ticks(d3.timeMonth.every(1)).tickFormat(d3.timeFormat('%B %Y')));
 
-    // Add the y-axis
     svg2.append("g")
       .call(d3.axisLeft(y));
 
-    // Create the line generator
     const line = d3.line()
       .x(d => x(d.date))
       .y(d => y(d.volume));
 
-    // Add the line path to the SVG element
     svg2.append("path")
       .datum(filteredData)
       .attr("fill", "none")
@@ -201,77 +275,8 @@ function loadDataAndDrawGraph2(year) {
       .style("font-size", "16px")
       .style("text-decoration", "underline")
       .text(`Trading Volume ($) for ${year}`);
-    
-      const annotations = [
-        {
-          note: {
-            label: "Important event",
-            title: "11/2/2019"
-          },
-          x: x(annotationDate),
-          y: y(yourYValue), // Replace 'yourYValue' with the actual y-value for the annotation
-          dx: 30,
-          dy: -30
-        }
-      ];
-    
-      const makeAnnotations = d3.annotation()
-        .annotations(annotations);
-    
-      svg2.append("g")
-        .attr("class", "annotation-group")
-        .call(makeAnnotations);
-  });
-}
-
-function loadannotations(year) {
-  // Load and Process Data
-  d3.csv("nvidia_stock.csv", d3.autoType).then(function(data) {
-    // Filter the data based on the selected year
-    const filteredData = data.filter(d => d.date.getFullYear().toString() === year);
-
-    // Define the x and y domains
-    x.domain(d3.extent(filteredData, d => d.date));
-    y.domain([0, d3.max(filteredData, d => d.high)]);
-
-    let specificDate = "2021-07-22"; // YYYY-MM-DD format
-    specificDate = d3.timeParse("%Y-%m-%d")(specificDate);
-    console.log(specificDate)
-
-    // Find the data point for the specific date
-    let specificDataPoint = filteredData.find(d => +d.date === +specificDate);
-
-    if (specificDataPoint) {
-      let correspondingValue = specificDataPoint.volume;
-
-      let xCoordinate = x(specificDate);
-      let yCoordinate = y(correspondingValue);
-
-      console.log("X Coordinate: ", xCoordinate);
-      console.log("Y Coordinate: ", yCoordinate);
-
-      // Create the annotation
-      const annotations = [{
-        note: {
-          label: "This is the high stock price for " + specificDate,
-          title: "Annotation for " + specificDate,
-        },
-        x: xCoordinate,
-        y: yCoordinate,
-        dy: 100,
-        dx: 100
-      }];
-
-      // Add annotation to the svg
-      const makeAnnotations = d3.annotation()
-        .annotations(annotations);
-
-        svg.call(makeAnnotations)
-          .selectAll(".annotation-note-label, .annotation-note-title")
-          .style("fill", "black") // Change the text color to black
-          .style("font-size", "16px"); // Change the font size to 16
-    } else {
-      console.log("No data for the specific date");
-    }
+      
+      const makeAnnotations = d3.annotation().annotations(annotation_vals[Number(year)- 2016])
+      svg2.append("g").call(makeAnnotations)
   });
 }
